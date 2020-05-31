33c304b95538c39b80c8d4bd65a41fb9e767f5f6
skandula106 <58414973+skandula106@users.noreply.github.com>
2020-05-31 16:04:15 +0530

    working with reactjs elements creating with jsx and without

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// two types elements creation in react js with jsx and without jsx, with jsx need compilation with babel
+// and without jsx we completley write in pure javascript
+
+// Createing react element using jsx
+const elementSWithJsx = (
+  <div className="textClass">
+    <p className="text-right">Understanding reactjs elements with jsx</p>
+    <h1>Started learning react js </h1>
+  </div>
+);
+// Createing react element using without jsx
+const elememntWithOutJsx = React.createElement(
+  "div",
+  { className: "textClass" },
+  React.createElement("p", null, "Understanding reactjs without jsx"),
+  React.createElement("h1", null, "Started learning react js")
 );
 
+ReactDOM.render(elementSWithJsx, document.getElementById("root"));
+ReactDOM.render(elememntWithOutJsx, document.getElementById("newElement"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
